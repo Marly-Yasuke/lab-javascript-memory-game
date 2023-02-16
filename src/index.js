021b6fd1f184789bdf89198863d9364f98ba68d6
Marly Diallo <marly.diallo2021@gmail.com>
2023-02-16 16:37:54 +0100

    check file message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// trying to run checkIfPair() method on both entries of memoryGame.pickedCards + when 2 cards are clicked turn them back
 
 const cards = [
   { name: "aquaman", img: "aquaman.jpg" },
@@ -28,32 +29,46 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+
 window.addEventListener("load", (event) => {
-  // let html = "";
-  // memoryGame.cards.forEach((pic) => {
-  //   html += `
-  //     <div class="card" data-card-name="${pic.name}">
-  //       <div class="back" name="${pic.img}"></div>
-  //       <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
-  //     </div>
-  //   `;
-  // });
+  let html = "";
+  memoryGame.cards.forEach((pic) => {
+    html += `
+      <div class="card" data-card-name="${pic.name}">
+        <div class="back" name="${pic.img}"></div>
+        <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+      </div>
+    `;
+  });
 
-  // // Add all the divs to the HTML
-  // document.querySelector("#memory-board").innerHTML = html;
+  // Add all the divs to the HTML
+  document.querySelector("#memory-board").innerHTML = html;
 
-  // // Bind the click event of each element to a function
-  // document.querySelectorAll(".card").forEach((card) => {
-  //   card.addEventListener("click", () => {
-  //     memoryGame.pickedCards.push(card)
-  //    card.classList.add('turned')
+  // Bind the click event of each element to a function
+  document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("click", () => {
+      memoryGame.pickedCards.push(card)
+      let firstCard = memoryGame.pickedCards[0]
+      let secondCard = memoryGame.pickedCards[1]
+     if(memoryGame.pickedCards.length == 2){
+console.log('pair')
+card.classList.add('turned')
+
+     }else if (memoryGame.pickedCards.length <= 2){
+      card.classList.add('turned')
+
+     }else if(memoryGame.pickedCards.length > 2){
+      card.classList.remove('turned')
+      memoryGame.pickedCards = []
+     }
+
+    //  use push method to push the cards into an array and use the method
 
 //  try loop into aray to run checkIfPairs
 // focus on the array manipulation
-
 //maybe try prevening event behavior to
 console.log('it works')
       console.log(`Card clicked: ${card}`);
-  //   });
-  // });
+    });
+  });
 });
